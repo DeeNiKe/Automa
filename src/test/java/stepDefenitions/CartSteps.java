1b7030a81cb967fdab3bf68fa0bd62de6c4cce95
DeeNike <it20126674@my.sliit.lk>
2025-03-12 20:47:22 +0530

    Creating TC_012

--- src/test/java/stepDefenitions/CartSteps.java
+++ src/test/java/stepDefenitions/CartSteps.java
@@ -3,13 +3,8 @@ package stepDefenitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class CartSteps {
 
